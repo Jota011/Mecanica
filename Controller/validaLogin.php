8142809274738d6f12179924028c9e9b5b843533
rangeladonai <kohei_adonai74739@outlook.com>
2023-02-22 22:08:09 -0300

    ajustes acontecendo

--- Controller/validaLogin.php
+++ Controller/validaLogin.php
@@ -1,8 +1,11 @@
 <?php
-if(isset($_POST['pinLogin']) && !empty($_POST['pinLogin'])){
-    $_SESSION['pinLogin'] = $_POST['pinLogin'];
-    include_once '../View/homeView.php';
-}else{
-    unset($_SESSION['pinLogin']);
-    header('Location:./View/loginView.php');
+
+if($_SERVER['REQUEST_METHOD'] == 'post'){
+    if(isset($_POST['pinLogin']) && !empty($_POST['pinLogin'])){
+        $_SESSION['pinLogin'] = $_POST['pinLogin'];
+        include_once '../View/homeView.php';
+    }else{
+        unset($_SESSION['pinLogin']);
+        header('Location:./View/loginView.php');
+    }
 }
--- View/loginView.php
+++ View/loginView.php
@@ -1,18 +1,22 @@
-<?php require_once '../Templates/header.php'; ?>
+<?php 
+require_once '../Templates/header.php'; 
+require '../Controller/validaLogin.php';
+?>
 <link rel="stylesheet" href="../Public/CSS/loginView.css">
 <body>
         <div class="hero is-fullheight">
                 <div class="hero-body is-justify-content-center is-align-items-center">
-                        <form action="../Controller/validaLogin.php" method="post">
+                        <form action="" method="post">
                                 <div class="columns is-flex is-flex-direction-column box">
                                         <div class="column">
                                                 <label for="pin">PIN PARA ACESSO</label>
                                                 <br>
-                                                <input class="input is-info" id="pin" type="text" name="pinLogin" placeholder="1234..." required>
+                                                <input class="input is-info" id="pin" type="password" name="pinLogin" placeholder="Q1W2E3R4..." required>
                                         </div>
                                         <div class="column">
                                                 <button class="button is-info is-fullwidth" type="submit" name="entrar">ENTRAR</button>
                                         </div>
+                                        <span class="tag is-info is-light">rangel_cabral@estudante.sesisenai.org.br</span>
                                 </div>
                         </form>
                 </div>
