da6bd5253679d3e10a142ce028c4e27baf03a0ce
rangeladonai <kohei_adonai74739@outlook.com>
2023-02-23 19:18:22 -0300

    ARQUIVO SQL DO DATABASE AJUSTADO

--- Model/SQL/radajori.sql
+++ Model/SQL/radajori.sql
@@ -1,55 +1,22 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 22-Fev-2023 às 23:43
--- Versão do servidor: 10.4.6-MariaDB
--- versão do PHP: 7.3.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE DATABASE radajori;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+USE radajori;
 
---
--- Banco de dados: `radajori`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `login`
---
-
-CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `senha` int(255) NOT NULL
+CREATE TABLE `usuariopin` (
+  `id_userpin` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `pin` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `motor`
---
 
 CREATE TABLE `motor` (
   `id_motor` int(11) NOT NULL,
   `descricao_motor` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `ordem_servico`
---
 
 CREATE TABLE `ordem_servico` (
   `id_os` int(11) NOT NULL,
@@ -58,14 +25,9 @@ CREATE TABLE `ordem_servico` (
   `data_os` date NOT NULL,
   `periodo` varchar(100) NOT NULL,
   `turma` varchar(100) NOT NULL,
+  `veiculo` varchar(255) not null, 
   `professor` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
 
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
@@ -74,87 +36,33 @@ CREATE TABLE `usuario` (
   `nivel_acesso` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `veiculos`
---
-
 CREATE TABLE `veiculos` (
   `id_veiculos` int(11) NOT NULL,
   `nome_veiculos` varchar(255) NOT NULL,
   `marca` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `motor`
---
 ALTER TABLE `motor`
   ADD PRIMARY KEY (`id_motor`);
 
---
--- Índices para tabela `ordem_servico`
---
 ALTER TABLE `ordem_servico`
   ADD PRIMARY KEY (`id_os`);
 
---
--- Índices para tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
+ALTER TABLE `usuariopin`
+  ADD PRIMARY KEY (`id_userpin`);
 
---
--- Índices para tabela `veiculos`
---
 ALTER TABLE `veiculos`
   ADD PRIMARY KEY (`id_veiculos`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
+ALTER TABLE `usuariopin`
+  MODIFY `id_userpin` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `motor`
---
 ALTER TABLE `motor`
   MODIFY `id_motor` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `ordem_servico`
---
 ALTER TABLE `ordem_servico`
   MODIFY `id_os` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `veiculos`
---
 ALTER TABLE `veiculos`
   MODIFY `id_veiculos` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
