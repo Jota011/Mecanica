ded0ec9ee451359534c9c9678176edfaf1700301
rangeladonai <kohei_adonai74739@outlook.com>
2023-03-07 15:17:00 -0300

    implementacao da TELA de cadastro de servico, cadServicoView, e ajuste na conexao com DB

--- View/cadServicoView.php
+++ View/cadServicoView.php
@@ -6,7 +6,8 @@ include_once '../Templates/header.php';
     <div class="container">
         <h2>Cadastro de Serviço</h2>
         <form action="">
-            <input type="date">
+            <label for="data">DATA</label>
+            <input type="date" id="data">
             <select name="turno" id="turno"> <!--TURNO É PERIODO NO DATABASE-->
                 <option value="">Selecione Turno</option>
                 <option value="Matutino">Matutino</option>
@@ -14,11 +15,22 @@ include_once '../Templates/header.php';
                 <option value="Noturno">Noturno</option>
             </select>
             <input type="text" name="turma" placeholder="Turma...">
-            <select name="veiculo" id="veiculo">
+            <select name="veiculo" id="veiculo" style="max-width:200px;">
                 <option value="">Selecione Veículo</option>
                 <?php
+                $sql = "SELECT * FROM veiculos";
+                $query = $mysqli->query($sql);
+                while ($row = mysqli_fetch_assoc($query)){
+                    echo $row;
+                    echo '<option value="'. $row['id_veiculo'] .'">' . $row['marca']. ' ' . $row['modelo'] . " - " .  $row['cor'] . " (" .$row['descricao'] . ")" . "</option>";
+                }
                 ?>
             </select>
+            <textarea placeholder="Atividades realizadas..." name="descricao" id="descricao" cols="30" rows="10" style="resize:none;"></textarea>
+            <input type="text" placeholder="Responsável pela aula" value="<?php echo isset($_SESSION['nome']) ? $_SESSION['nome'] : ''; ?>">
+            <div id="botoes">
+                <input type="submit" value="Cadastrar Serviço">
+            </div>
         </form>
     </div>
 </body>
