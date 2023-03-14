798745141a57396b44b264ae9f62af8a5aefbfeb
rangeladonai <kohei_adonai74739@outlook.com>
2023-03-13 21:41:30 -0300

    ajuste

---
+++ Model/connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'radajori';
+$username = 'root';
+$psswrd = '';
+
+$mysqli = mysqli_connect($host,$username,$psswrd,$dbname);
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}else{
+//    echo 'conectado';
+}
